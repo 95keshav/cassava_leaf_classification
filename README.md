3cf7e5937f1caf48e39bab55683b101341add2db
Keshav Sharma <kshar26@uwo.ca>
2021-12-10 09:58:38 -0500

    add models

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# cassava_leaf_classification
+# **CASSAVA LEAF DISEASE DETECTION**
+
+-   Classification of leaf disease of the cassava plant.
+    
+-   Cassava is major food crop grown in Africa.
+    
+-   CNN’s are usually used in case of image classification due to their ability to extract higher order features in the images.
+    
+-   Custom CNN implementations.
+    
+-   2 pretrained models VGG-16 and Resnet50 .
+
+
+# **Understanding the Problem**
+
+-   Cassava is the third largest source of carbohydrates for human food in the world but is vulnerable to virus diseases, which threaten to destabilize food security in sub-Saharan Africa.
+    
+-   The diseases affect the overall production of cassava plant crop which leads to food shortage in Africa .
+    
+-   Existing methods need a manual inspection of plants which is very slow and costly and will adversely affect production and farmer’s livelihood
+
+## **Project objective**
+
+-   Create a machine learning model which can detect the disease pattern at very early stage.
+    
+-   With the help of Machine Learning Models, it is possible to identify common diseases so they can be treated.
+    
+-   Image classification algorithms in machine learning can predict whether a particular cassava plant leaf has a disease pattern or not.
+    
+-   It will be a fast and inexpensive way to identify leaf diseases and can reach to the larger population in less time.
+
+## **Dataset**
+
+-   The original dataset contained around 21k images of cassava leaves of size 800x600.
+    
+-   We only used only 1000 colored images for training and 100 images for testing .
+
+- [Dataset](https://www.kaggle.com/c/cassava-leaf-disease-classification/data)
+    
+### These images are classified into five classes :
+    
+
+-   Cassava Bacterial Blight (CBB)
+    
+-   Cassava Brown Streak Disease (CBSD)
+    
+-   Cassava Green Mottle (CGM)
+    
+-   Cassava Mosaic Disease (CMD)
+    
+-   Healthy.
---
+++ image_preprocessing.py
@@ -0,0 +1,61 @@
+import env
+import pandas as pd
+import numpy as np
+from PIL import Image
+from keras.utils import np_utils
+from matplotlib import pyplot as plt
+
+
+class ImagePreProcessing:
+    def __init__(self, csv_path, img_limit):
+        try:
+            if csv_path:
+                self.csv = pd.read_csv(csv_path)
+                self.x_data = []
+                self.y_data = []
+                if type(img_limit).__name__ == "int":
+                    self.img_limit = int(img_limit)
+                else:
+                    self.img_limit = self.csv.shape[0]
+        except Exception as e:
+            print(e)
+
+    def processing(self, image_path, size=(224, 224)):
+        for index, row in self.csv.iterrows():
+            image = Image.open(f"{image_path}{row['image_id']}")
+            image = image.resize(size)
+            img_arr = np.array(image, dtype="float32")
+            img_arr /= 255
+            self.x_data.append(img_arr)
+            self.y_data.append(row["label"])
+            print(index)
+            if index >= self.img_limit:
+                break
+
+    def save_data(self, feat_dest, target_dest):
+        np.save(feat_dest, np.array(self.x_data))
+        n_classes = 5
+        self.y_data = np_utils.to_categorical(self.y_data, n_classes)
+        np.save(target_dest, np.array(self.y_data))
+
+    def process(self, image_path, feat_dest, target_dest, size=(224, 224)):
+        self.processing(image_path, size)
+        self.save_data(feat_dest, target_dest)
+
+
+base_path = env.BASEPATH
+image_limit = str(env.IMAGE_COUNT)
+csv_path = env.CSV
+image_path = f"{base_path}train_images/"
+feat_dest = env.FEATURES_PATH
+target_dest = env.LABELS_PATH
+
+preprocessing = ImagePreProcessing(csv_path, 5000)
+preprocessing.process(image_path, feat_dest, target_dest)
+
+x_features = np.load(env.FEATURES_PATH)
+y_labels = np.load(env.LABELS_PATH)
+
+plt.imshow(x_features[3])
+plt.show()
+print(x_features[3].shape)
